acbb9b84010d1465bffc290e2e450dcfbccf6cc8
Teodor Tomic <teodor.tomic@gmail.com>
2019-04-18 00:51:19 +0200

    Minor adjustment to arrow keys

--- keyboards/tada68/keymaps/kbp-v60/keymap.c
+++ keyboards/tada68/keymaps/kbp-v60/keymap.c
@@ -28,8 +28,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,  KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,  KC_9,    KC_0,    KC_MINS,  KC_EQL,  KC_BSPC, KC_DEL, \
   KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,  KC_O,    KC_P,    KC_LBRC,  KC_RBRC, KC_BSLS, KC_HOME,\
   MO(_FL), KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,  KC_L,    KC_SCLN, KC_QUOT,           KC_ENT,  KC_PGUP,\
-  KC_LSFT,         KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,  KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, KC_UP,   KC_PGDN,\
-  KC_LCTL, KC_LGUI,KC_LALT,                KC_SPC,                        MO(_FL), KC_ALGR, KC_RGUI,  KC_LEFT, KC_DOWN, KC_RGHT),
+  KC_LSFT,         KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,  KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, KC_RSFT, KC_PGDN,\
+  KC_LCTL, KC_LGUI,KC_LALT,                KC_SPC,                        MO(_FL), KC_ALGR, KC_LEFT,  KC_DOWN, KC_UP,   KC_RGHT),
 
   /* Keymap _FL: Function Layer
    * ,----------------------------------------------------------------.
@@ -49,5 +49,5 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_CAPS, _______, KC_UP,   _______, KC_BSPC, KC_PSCR, KC_PGUP, KC_HOME, KC_END,  KC_PAUS, KC_UP,   _______, _______, _______, KC_END, \
   _______, KC_LEFT, KC_DOWN, KC_RIGHT,KC_DEL,  KC_SLCK, KC_PGDN, KC_TLDE, KC_INS,  KC_LEFT, KC_DOWN, KC_RIGHT,         _______, KC_PSCR,\
   _______,          _______, _______, BL_DEC,  BL_TOGG, BL_INC,  _______, KC_VOLD, KC_VOLU, KC_MUTE, _______, KC_BTN1, KC_MS_U, KC_BTN2,\
-  _______, _______, _______,                   _______,                            _______, _______, _______, KC_MS_L, KC_MS_D, KC_MS_R),
+  _______, _______, KC_RGUI,                   _______,                            _______, _______, _______, KC_MS_L, KC_MS_D, KC_MS_R),
 };
