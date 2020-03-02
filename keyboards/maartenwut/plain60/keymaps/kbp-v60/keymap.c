09b44ee515fa04975438a1dfe75f8585e2f9453f
Teodor Tomic <teodor.tomic@gmail.com>
2020-03-02 01:48:30 -0800

    add numpad on wasd

--- keyboards/maartenwut/plain60/keymaps/kbp-v60/keymap.c
+++ keyboards/maartenwut/plain60/keymaps/kbp-v60/keymap.c
@@ -20,8 +20,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_FN] = LAYOUT(
   KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_DEL, \
-  KC_CAPS, _______, KC_UP,   _______, KC_BSPC, KC_PSCR, KC_PGUP, KC_HOME, KC_END,  KC_HOME, KC_UP,   KC_END,  _______, KC_PGUP,\
-  _______, KC_LEFT, KC_DOWN, KC_RIGHT,KC_DEL,  KC_SLCK, KC_PGDN, KC_TILD, KC_INS,  KC_LEFT, KC_DOWN, KC_RIGHT,_______, KC_PGDN,\
-  _______, _______, _______, _______, _______, _______, _______, _______, KC_VOLD, KC_VOLU, KC_MUTE, _______, KC_BTN1, _______,\
-  _______, _______, _______,                   _______,                            _______, _______, KC_MS_L, KC_MS_D),
+  KC_CAPS, KC_7,    KC_8,    KC_9,    KC_BSPC, KC_PSCR, KC_PGUP, KC_HOME, KC_END,  KC_HOME, KC_UP,   KC_END,  _______, KC_PGUP,\
+  _______, KC_4,    KC_5,    KC_6,    KC_DEL,  KC_SLCK, KC_PGDN, KC_TILD, KC_INS,  KC_LEFT, KC_DOWN, KC_RIGHT,_______, KC_PGDN,\
+  _______, KC_1,    KC_2,    KC_3,    _______, _______, _______, _______, KC_VOLD, KC_VOLU, KC_MUTE, _______, KC_BTN1, _______,\
+  _______, _______, KC_0,                      _______,                            _______, _______, KC_MS_L, KC_MS_D),
 };
